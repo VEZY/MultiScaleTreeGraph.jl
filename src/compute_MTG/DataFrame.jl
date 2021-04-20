ac0fe3e25784d0ef44c03f70ad7917009a3329ca
remi.vezy <VEZY@users.noreply.github.com>
2021-04-20 15:42:10 +0200

    Update DataFrame.jl

--- src/compute_MTG/DataFrame.jl
+++ src/compute_MTG/DataFrame.jl
@@ -1,18 +1,39 @@
-function DataFrames.DataFrame(node::Node,vars::T) where T <: Union{AbstractArray, Tuple}
-    node_vec = get_printing(node)
+"""
+    DataFrame(mtg::Node,vars::T[,type::Union{Union,DataType}=Any])
+
+Convert an MTG into a DataFrame.
+
+# Arguments
+
+- `mtg::Node`: An mtg node (usually the root node).
+- `key`: The key, or attribute name. Used to list the variables that must be added to the
+`DataFrame`. It is given either as Symbols (faster) or String, or an Array of (or a Tuple).
+
+# Examples
+
+```julia
+# Importing the mtg from the github repo:
+mtg,classes,description,features =
+    read_mtg(download("https://raw.githubusercontent.com/VEZY/MTG.jl/master/test/files/simple_plant.mtg"))
+
+DataFrame(mtg, :Length)
+```
+"""
+function DataFrames.DataFrame(mtg::Node,key::T) where T <: Union{AbstractArray, Tuple}
+    node_vec = get_printing(mtg)
     df = DataFrame(tree = node_vec)
 
-    for var in vars
-        insertcols!(df, var => [descendants(node,var,self=true)...])
+    for var in key
+        insertcols!(df, var => [descendants(mtg,var,self=true)...])
     end
     df
 end
 
-function DataFrames.DataFrame(node::Node,vars::T) where T <: Symbol
-    DataFrame([get_printing(node), [descendants(node,vars,self=true)...]],[:tree,vars])
+function DataFrames.DataFrame(mtg::Node,key::T) where T <: Symbol
+    DataFrame([get_printing(mtg), [descendants(mtg,key,self=true)...]],[:tree,key])
 end
 
-function DataFrames.DataFrame(node::Node,vars::T) where T <: AbstractString
-    vars = Symbol(vars)
-    DataFrame([get_printing(node), [descendants(node,vars,self=true)...]],[:tree,vars])
+function DataFrames.DataFrame(mtg::Node,key::T) where T <: AbstractString
+    key = Symbol(key)
+    DataFrame([get_printing(mtg), [descendants(mtg,key,self=true)...]],[:tree,key])
 end
