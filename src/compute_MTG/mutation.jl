15b5c15f6daaf6b96ede3c7dbb1ec47b480e8ac4
remi.vezy <VEZY@users.noreply.github.com>
2021-04-26 20:48:18 +0200

    Update mutation.jl
    Add expression formatting to match node.var or node.attributes.var in case of attributes variables: adds macro @format_expr

--- src/compute_MTG/mutation.jl
+++ src/compute_MTG/mutation.jl
@@ -25,3 +25,62 @@ end
 function Base.append!(node::Node{NodeMTG, <:AbstractDict}, attr)
     merge!(node.attributes, Dict(zip(keys(attr),values(attr))))
 end
+
+
+
+macro format_expr(node, args...)
+    arguments = (args...,)
+    rewrite_expr!(arguments)
+    println(arguments)
+end
+
+"""
+    rewrite_expr!(arguments)
+
+Re-write the call to the variables of a node in an expression to match their location: leave
+it as it is if the variable is a node field, or add `attributes` after the node if it is
+an attribute.
+
+# Examples
+
+```
+>julia test = :(x = node.name)
+>julia rewrite_expr!(test)
+>julia test
+:(node.attributes.x = node.name)
+
+>julia test = :(x = node.foo)
+>julia rewrite_expr!(test)
+>julia test
+:(node.attributes.x = node.attributes.foo)
+```
+"""
+function rewrite_expr!(arguments::Expr)
+
+    # For the Left-Hand Side (LHS)
+    if isa(arguments,Expr) && arguments.head == :(=) && isa(arguments.args[1],Symbol)
+        arguments.args[1] = :(node.attributes.$(arguments.args[1]))
+        # if !(Symbol(replace(arg,"node."=>"")) in fieldnames(Node))
+        # x.args[1] = :(node.attributes)
+    end
+
+    # For the RHS:
+    for x in arguments.args
+        arg = string(x)
+        if isa(x,Expr) && x.head == :. && occursin("node.",arg) && !(Symbol(replace(arg,"node."=>"")) in fieldnames(Node))
+            x.args[1] = :(node.attributes)
+        else
+            rewrite_expr!(x)
+        end
+    end
+end
+
+function rewrite_expr!(arguments)
+    nothing
+end
+
+function rewrite_expr!(arguments::Tuple)
+    for x in arguments
+        rewrite_expr!(x)
+    end
+end
