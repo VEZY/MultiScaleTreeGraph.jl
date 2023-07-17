56980144c9d39959c329574647f7864c16af255e
Rémi Vezy <VEZY@users.noreply.github.com>
2023-07-17 15:14:02 +0200

    Update check_filters.jl
    optimize is_filtered

--- src/compute_MTG/check_filters.jl
+++ src/compute_MTG/check_filters.jl
@@ -11,7 +11,7 @@ check_filters(mtg, scale = (1,2))
 check_filters(mtg, scale = (1,2), symbol = "Leaf", link = "<")
 ```
 """
-function check_filters(node; scale = nothing, symbol = nothing, link = nothing)
+function check_filters(node; scale=nothing, symbol=nothing, link=nothing)
 
     root_node = get_root(node)
 
@@ -54,26 +54,29 @@ end
 
 Is a node filtered in ? Returns `true` if the node is kept, `false` if it is filtered-out.
 """
-function is_filtered(node, scale, symbol, link, filter_fun)
+@inline function is_filtered(node, scale, symbol, link, filter_fun)
 
-    link_keep = is_filtered(link, node.MTG.link)
-    symbol_keep = is_filtered(symbol, node.MTG.symbol)
-    scale_keep = is_filtered(scale, node.MTG.scale)
+    link_keep = isnothing(link) || is_filtered(link, node.MTG.link)
+    symbol_keep = isnothing(link) || is_filtered(symbol, node.MTG.symbol)
+    scale_keep = isnothing(link) || is_filtered(scale, node.MTG.scale)
     filter_fun_keep = isnothing(filter_fun) || filter_fun(node)
 
     scale_keep && symbol_keep && link_keep && filter_fun_keep
 end
 
-
-function is_filtered(filter, value)
-    isnothing(filter) || value in filter
+@inline function is_filtered(filter::Nothing, value)
+    true
 end
 
-function is_filtered(filter::String, value)
-    isnothing(filter) || value in (filter,)
+@inline function is_filtered(filter, value)
+    value in filter
 end
 
-function is_filtered(filter, value::T) where {T<:Union{Tuple,Array}}
+@inline function is_filtered(filter::String, value)
+    value in (filter,)
+end
+
+@inline function is_filtered(filter, value::T) where {T<:Union{Tuple,Array}}
     all(map(x -> is_filtered(filter, x), value))
 end
 
